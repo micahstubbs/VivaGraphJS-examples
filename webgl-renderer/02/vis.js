468ad97548cda084f807eee8f40cab03443d2a74
@micahstubbs <micah.stubbs@gmail.com>
2018-06-21 16:57:40 -0700

    modernize js with lebab

--- webgl-renderer/02/vis.js
+++ webgl-renderer/02/vis.js
@@ -1,11 +1,11 @@
 /*global Viva*/
-var graphGenerator = Viva.Graph.generator()
+const graphGenerator = Viva.Graph.generator();
 
 // var graph = graphGenerator.randomNoLinks(500);
 // var graph = graphGenerator.completeBipartite(100, 1);
 // var graph = graphGenerator.complete(2);
 // var graph = graphGenerator.grid(100, 100);
-var graph = graphGenerator.balancedBinTree(10)
+const graph = graphGenerator.balancedBinTree(10);
 // var graph = graphGenerator.ladder(1000);
 // var graph = Viva.Graph.graph();
 //graph.addLink(1, 2);
@@ -14,21 +14,21 @@ console.log('graph', graph)
 console.log('nodes', graph.getNodesCount())
 console.log('links', graph.getLinksCount())
 
-var layout = Viva.Graph.Layout.forceDirected(graph, {
+const layout = Viva.Graph.Layout.forceDirected(graph, {
   springLength: 30,
   springCoeff: 0.0008,
   dragCoeff: 0.01,
   gravity: -1.2,
   theta: 1
-})
+});
 
-var graphics = Viva.Graph.View.webglGraphics()
+const graphics = Viva.Graph.View.webglGraphics();
 
-var renderer = Viva.Graph.View.renderer(graph, {
-  layout: layout,
-  graphics: graphics,
+const renderer = Viva.Graph.View.renderer(graph, {
+  layout,
+  graphics,
   renderLinks: true,
   prerender: true
-})
+});
 
 renderer.run()
